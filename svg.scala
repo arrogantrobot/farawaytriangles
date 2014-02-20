68441e365de72f403a0debc458f5a5dffdb1af62
arrogantrobot <arrogantrobot@gmail.com>
2014-02-20 00:02:00 -0600

    preparing to go full triangle on this

--- svg.scala
+++ svg.scala
@@ -10,25 +10,20 @@ class Triangle(val len: Double, val x1: Double, val y1: Double) {
   val y3 = y1 + sqrt3/2 * len
   val coordinateString = x1+","+y1+" "+x2+","+y2+" "+x3+","+y3
   def getXML = <polygon points={coordinateString} style="fill:rgb(0,0,0);stroke-width:1;stroke:rgb(0,0,0)" />
-  def unapply(c: coords) = try {
-    Some(new Triangle(c.len, c.x1, c.y1))
-  } catch {
-    case ex: Exception => None
-  }
 }
 
 
-class coords(val len: Double, val x1:Double, val y1:Double) {
+case class coords(val len: Double, val x1:Double, val y1:Double) {
   override def toString = len +" "+ x1 +" "+ y1
 }
 
 object SVG {
   val sqrt3 = 1.7320508075688772
-  val tr = new Triangle(100, 100,0)
-  def getSvg = 
+  def getSvg(tr: Triangle):Unit = {
     <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
       {tr.getXML}
     </svg>
+  }
 
   /*def triTriangles(tri: Triangle):List[Triangle] = {
     List(Triangle(1,1,1))
@@ -52,7 +47,7 @@ object SVG {
 
   def output():Unit = {
     val thing = new coords(100, 100, 0)
-    println(getSvg)
+    println(getSvg(thing))
     makeMagics(new coords(1000, 1000, 0), List(), 1) map {
       println(_)
     }
