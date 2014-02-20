728abbd4adb3d251cf64ea3760a4251e272c1b98
arrogantrobot <arrogantrobot@gmail.com>
2014-02-20 07:41:17 -0600

    reformat for readability

--- svg.scala
+++ svg.scala
@@ -10,40 +10,49 @@ class Triangle(val len: Double, val x1: Double, val y1: Double) {
   val x3 = x1 - len/2
   val y3 = y1 + sqrt3/2 * len
   val coordinateString = x1+","+y1+" "+x2+","+y2+" "+x3+","+y3
-  def getXML = <polygon points={coordinateString} style="fill:rgb(0,0,0);stroke-width:0;stroke:rgb(0,0,0)" />
+  val style = "fill:rgb(0,0,0);stroke-width:0;stroke:rgb(0,0,0)"
+  def getXML = <polygon points={coordinateString} style={style} />
   override def toString = len +" "+ x1 +" "+ y1
 }
 
 object SVG {
   val sqrt3 = 1.7320508075688772
   def getSvg(objects: List[Triangle]):Elem = {
-    val xml = <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-      { objects map {_.getXML} }
-    </svg>
+    val xml = 
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+        { objects map {_.getXML} }
+      </svg>
     xml
   }
 
   def bigLeft(triangle: Triangle):List[Triangle] = {
-    List(new Triangle(triangle.len/2,
-               triangle.x1 - triangle.len * .25,
-               triangle.y1 + (triangle.len/2*sqrt3/2)
-             )
+    List(
+      new Triangle(triangle.len/2,
+        triangle.x1 - triangle.len * .25,
+        triangle.y1 + (triangle.len/2*sqrt3/2)
+      )
     )
   }
 
   def triforce(triangle: Triangle):List[Triangle] = {
     List(new Triangle(triangle.len/4, triangle.x1, triangle.y1),
-      new Triangle(triangle.len/4, triangle.x1 - (triangle.len/8), triangle.y1 + (triangle.len/4*sqrt3/2)),
-      new Triangle(triangle.len/4, triangle.x1 + (triangle.len/8), triangle.y1 + (triangle.len/4*sqrt3/2)))
+      new Triangle(triangle.len/4,
+        triangle.x1 - (triangle.len/8),
+        triangle.y1 + (triangle.len/4*sqrt3/2)),
+      new Triangle(triangle.len/4,
+        triangle.x1 + (triangle.len/8),
+        triangle.y1 + (triangle.len/4*sqrt3/2))
+    )
   }
 
   def iterate(triangle: Triangle, dims: List[Triangle], iters: Int):List[Triangle] = {
     if (iters == 0) dims ++ List(triangle)
     else  {
-      val smallTriangle = new Triangle(triangle.len/2, 
-                                  triangle.x1 + triangle.len * .25,
-                                  triangle.y1 + (triangle.len/2*sqrt3/2))
-
+      val smallTriangle = 
+        new Triangle(triangle.len/2, 
+          triangle.x1 + triangle.len * .25,
+          triangle.y1 + (triangle.len/2*sqrt3/2)
+        )
       dims ++ bigLeft(triangle) ++ triforce(triangle) ++ iterate(smallTriangle, dims, iters -1)
     }
   }
