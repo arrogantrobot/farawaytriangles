8e4fcf79436dd6d73c0124cf57bed66aad8fda8f
rlong <rob@buildtall.com>
2015-09-26 23:58:05 -0500

    update to do dual branching

--- build.sbt
+++ build.sbt
@@ -15,3 +15,18 @@ scalacOptions ++= Seq(
   "-Yinline",
   "-Yno-adapted-args",
   "-encoding", "utf8")
+
+libraryDependencies := {
+  CrossVersion.partialVersion(scalaVersion.value) match {
+    // if scala 2.11+ is used, add dependency on scala-xml module
+    case Some((2, scalaMajor)) if scalaMajor >= 11 =>
+      libraryDependencies.value ++ Seq(
+        "org.scala-lang.modules" %% "scala-xml" % "1.0.3"
+      )
+    case _ =>
+      // or just libraryDependencies.value if you don't depend on scala-swing
+      libraryDependencies.value
+  }
+}
+
+seq(Revolver.settings: _*)
--- svg.scala
+++ svg.scala
@@ -1,4 +1,7 @@
 package com.farawaylabs.scala.logo
+
+import java.io.{File, PrintWriter}
+
 import scala.xml.Elem
 
 case class Triangle(val len: Double, val x1: Double, val y1: Double) {
@@ -13,50 +16,67 @@ case class Triangle(val len: Double, val x1: Double, val y1: Double) {
   override def toString = len +" "+ x1 +" "+ y1
 }
 
+object Triangle {
+  val tsf = 4
+  def getRightSmallTriangle(triangle:Triangle):Triangle = {
+    new Triangle(triangle.len/2,
+      triangle.x1 - triangle.len * .25,
+      triangle.y1 + (triangle.len/2*triangle.sqrt3/2)
+    )
+  }
+  def getLeftSmallTriangle(triangle:Triangle):Triangle = {
+    new Triangle(triangle.len/2,
+      triangle.x1 + triangle.len * .25,
+      triangle.y1 + (triangle.len/2*triangle.sqrt3/2)
+    )
+  }
+  def getSmallTriangle(triangle:Triangle):Triangle = {
+    new Triangle(triangle.len/2,
+      triangle.x1,
+      triangle.y1 //+ (triangle.len/2*triangle.sqrt3/2)
+    )
+  }
+  def triforce(triangle: Triangle):List[Triangle] = {
+    List(new Triangle(triangle.len/tsf, triangle.x1, triangle.y1),
+      new Triangle(triangle.len/tsf,
+        triangle.x1 - (triangle.len/(tsf*2)),
+        triangle.y1 + (triangle.len/tsf*triangle.sqrt3/2)),
+      new Triangle(triangle.len/tsf,
+        triangle.x1 + (triangle.len/(tsf*2)),
+        triangle.y1 + (triangle.len/tsf*triangle.sqrt3/2))
+    )
+  }
+}
+
 object SVG {
   val sqrt3 = 1.7320508075688772
   def getSvg(objects: List[Triangle]):Elem = {
-    val xml = 
+    println(s"drawing ${objects.length} triangles")
+    val xml =
       <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-         objects map _.getXML
+        { objects.map(_.getXML) }
       </svg>
     xml
   }
 
-  def bigLeft(triangle: Triangle):List[Triangle] = {
-    List(
-      new Triangle(triangle.len/2,
-        triangle.x1 - triangle.len * .25,
-        triangle.y1 + (triangle.len/2*sqrt3/2)
-      )
-    )
-  }
-
-  def triforce(triangle: Triangle):List[Triangle] = {
-    List(new Triangle(triangle.len/4, triangle.x1, triangle.y1),
-      new Triangle(triangle.len/4,
-        triangle.x1 - (triangle.len/8),
-        triangle.y1 + (triangle.len/4*sqrt3/2)),
-      new Triangle(triangle.len/4,
-        triangle.x1 + (triangle.len/8),
-        triangle.y1 + (triangle.len/4*sqrt3/2))
-    )
-  }
-
   def iterate(triangle: Triangle, dims: List[Triangle], iters: Int):List[Triangle] = {
-    if (iters == 0) dims ++ List(triangle)
+    if (iters == 0) dims ++ Triangle.triforce(triangle)
     else  {
-      val smallTriangle = 
-        new Triangle(triangle.len/2, 
-          triangle.x1 + triangle.len * .25,
-          triangle.y1 + (triangle.len/2*sqrt3/2)
-        )
-      dims ++ bigLeft(triangle) ++ triforce(triangle) ++ iterate(smallTriangle, dims, iters -1)
+      dims ++
+        Triangle.triforce(triangle) ++
+        iterate(Triangle.getRightSmallTriangle(triangle), dims, iters - 1) ++
+        iterate(Triangle.getLeftSmallTriangle(triangle), dims, iters - 1)
+      /*
+        iterate(Triangle.getRightSmallTriangle(triangle), dims, iters -1)*/
     }
   }
 
-  def output(len: Double = 1000.0, iterations: Int = 256):Unit = {
-    val tris = iterate(new Triangle(len, len / 2, 0.0), List(), iterations)
+  def output(len: Double = 1000.0, iterations: Int = 10):Unit = {
+    val tris = iterate(new Triangle(len, len /2, 0.0), List(), iterations)
+    val writer = new PrintWriter(new File("triforce.svg"))
+    writer.write(getSvg(tris).toString())
+    writer.close()
+
     println(getSvg(tris))
   }
 
